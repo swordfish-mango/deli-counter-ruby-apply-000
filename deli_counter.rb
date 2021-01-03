d21282bbdb898de97d920d486a355e7496d523d9
Marcus Shimotsu <marcus.shimotsu@gmail.com>
2021-01-03 03:10:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ katz_deli = []
 def line(ary)
   line_string = "The line is currently: "
   ary.each do |name, place|
-    line_string += "#{(place.to_i+1).to_str}. #{name} "
+    line_string += "#{(place.to_i+1).to_s}. #{name} "
   end
   if ary.empty?
     puts "The line is currently empty."
