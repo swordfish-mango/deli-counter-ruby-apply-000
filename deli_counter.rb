fbe410111407a2ee772f2da325457a0527d7b37c
Marcus Shimotsu <marcus.shimotsu@gmail.com>
2021-01-03 03:00:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+katz_deli = []
+
+def line(ary)
+  line_string = "The line is currently: "
+  ary.each do |name, place|
+    line_string += "#{place}. #{name} "
+  end
+  if ary == []
+    "The line is currently empty."
+  else
+    line_string
+  end
+end
+
+def take_a_number(ary,name)
+  ary.append(name)
+  puts "Welcome, #{name}. You are number #{ary.length} in line"
+end
+
